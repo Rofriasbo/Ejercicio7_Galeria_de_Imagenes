51d23dcf4d86ea90bd0922f11d0d61240fe0cbe9
Azaral <alazamdel@gmail.com>
2024-09-08 14:34:37 -0700

    JS

---
+++ ejercicio7.js
@@ -0,0 +1,5 @@
+// Función para cambiar la imagen principal al hacer clic en una miniatura
+function changeImage(newSrc) {
+    const mainImage = document.getElementById('mainImage');
+    mainImage.src = newSrc;
+}
